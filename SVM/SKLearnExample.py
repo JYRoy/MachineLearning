c6bcd18e2201dc2f3c5e967577fc473be97b6638
jyroy <jyroooy@163.com>
2019-12-20 20:50:54 +0800

    SVM

--- SVM/SKLearnExample.py
+++ SVM/SKLearnExample.py
@@ -6,21 +6,23 @@ import numpy as np
 import pylab as pl
 from sklearn import svm
 
-# 创建一些随机的点
-np.random.seed(0)
-X = np.r_[np.random.randn(20, 2) - [2, 2], np.random.randn(20, 2) + [2, 2]]
-Y = [0] * 20 + [1] * 20
+# 创建40个随机的点
+np.random.seed(0) #取固定的一些随机数
+X = np.r_[np.random.randn(20, 2) - [2, 2], np.random.randn(20, 2) + [2, 2]] # 让两组点分别靠左和靠右，np.r_是按列连接两个矩阵，就是把两矩阵上下相加，要求列数相等。rand函数根据给定维度生成[0,1)之间的数据，包含0，不包含1
+Y = [0] * 20 + [1] * 20   # 赋给标记
 
-
+# 实例化SVM分类器
 clf = svm.SVC(kernel='linear')
 clf.fit(X, Y)
 
-# 把图画出来
-w = clf.coef_[0]
-a = -w[0] / w[1]
-xx = np.linspace(-5, 5)
-yy = a * xx - (clf.intercept_[0]) / w[1]
+# 求出超平面
+# w0*x + w1*y + w3 = 0 可以转化成点斜式 y = -(w0/w1)*x + (w3/w1)
+w = clf.coef_[0] # coef_为w1到wn
+a = -w[0] / w[1]  #斜率
+xx = np.linspace(-5, 5) # 产生 -5 到 5 中连续的值
+yy = a * xx - (clf.intercept_[0]) / w[1]   # intercept_为w3
 
+# 画出和支持向量点相切的两条直线
 b = clf.support_vectors_[0]
 yy_down = a * xx + (b[1] - a * b[0])
 b = clf.support_vectors_[-1]
@@ -35,9 +37,8 @@ pl.plot(xx, yy, 'k-')
 pl.plot(xx, yy_down, 'k--')
 pl.plot(xx, yy_up, 'k--')
 
-pl.scatter(clf.support_vectors_[:, 0], clf. support_vectors_[:, 1],
-           s=80, facecolors='none')
+# 画出所有的点
 pl.scatter(X[:, 0], X[:, 1], c=Y, cmap=pl.cm.Paired)
-
+# 生成图像
 pl.axis('tight')
 pl.show()

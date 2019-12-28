35a29abf554bad64db4219f4662ee0f0399017db
jyroy <jyroooy@163.com>
2019-12-28 17:09:34 +0800

    利用XOR关系的一组数据进行的NNImplement简单测试

---
+++ NN/SimpleTest.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Author: JYRoooy
+
+from NeuralNetworkImplement import NeuralNetwork
+import numpy as np
+if __name__ == '__main__':
+    nn = NeuralNetwork([2, 2, 1], 'tanh')  # 输入层2个神经元， 隐藏层2个神经元， 输出层1个神经单元
+    X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+    Y = np.array([0, 1, 1, 0])
+    nn.fit(X, Y)
+    for i in [[0, 0], [0, 1], [1, 0], [1, 1]]:
+        print(i, nn.predict(i))
+

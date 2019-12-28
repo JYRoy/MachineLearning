533935bf697c7bfb9d2a2de875bab435851aa625
jyroy <jyroooy@163.com>
2019-12-28 18:40:53 +0800

    利用XOR关系的数据集测试

--- NN/SimpleTest.py
+++ NN/SimpleTest.py
@@ -4,6 +4,9 @@
 
 from NeuralNetworkImplement import NeuralNetwork
 import numpy as np
+'''
+利用简单非线性关系的数据集测试Implement
+'''
 if __name__ == '__main__':
     nn = NeuralNetwork([2, 2, 1], 'tanh')  # 输入层2个神经元， 隐藏层2个神经元， 输出层1个神经单元
     X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
